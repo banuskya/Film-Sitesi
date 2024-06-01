591d61bd3df176a183688182fd393ed2f7f8026a
banuskya <nurbanukayar108@gmail.com>
2024-06-01 15:00:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const arrows = document. querySelectorAll("-arrow");
+const novieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+    const ımageItem = movieLists[i].querySelectorAll("img").lenght;
+    arrow.addEvenListener("click", function (){
+    movieLists[i].style.transform= ``
+    });
+
+});
